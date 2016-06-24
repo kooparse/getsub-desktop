4581254b8344e250ada27bd8eb6d0cf14bb77276
alex <kooparse@protonmail.com>
2016-06-24 03:10:46 +0200

    subtitle list sorted by score (thanks fuse.js)

--- renderer/utils/getsub/index.js
+++ renderer/utils/getsub/index.js
@@ -1,19 +1,38 @@
 /* API(s) */
+import Fuse from 'fuse.js'
 import opensubtitle from './opensubtitle'
 
 /**
  * Getsub Search API
- *
  *
  * @param {Object} File object containing our file path and name
  * @param {String} Search language filter
  * @return {Object} Resulted search object (with subtitles list and origin names)
  */
 export const search = async (file, lang) => {
-  const subtitles = await opensubtitle(file, lang)
+  let subtitles = await opensubtitle(file, lang)
+
+  /**
+   * We use Fuse (https://github.com/krisk/Fuse)
+   * to comparing the file name with our list of
+   * subtitles found.
+   *
+   * @return {Array} A new array sorted by score
+   */
+  const fuse = new Fuse(subtitles, {
+    caseSensitive: false,
+    includeScore: false,
+    shouldSort: true,
+    tokenize: false,
+    threshold: 0.6,
+    location: 0,
+    distance: 100,
+    maxPatternLength: file.name.length + 8,
+    keys: ['fileName']
+  })
 
   return {
-    subtitles,
+    subtitles: fuse.search(file.name),
     filePath: file.path,
     originName: file.name,
     originLang: lang
